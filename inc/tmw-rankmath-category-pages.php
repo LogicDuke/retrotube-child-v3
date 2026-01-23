a14abe01e3bb6243b328386ad1af1271c03adb20
LogicDuke <fierensstefan08@gmail.com>
2026-01-23 15:23:30 +0100

    [TMW-FIX] [TMW-RM-AUDIT] Log invalid RankMath accessible post types

--- inc/tmw-rankmath-category-pages.php
+++ inc/tmw-rankmath-category-pages.php
@@ -24,3 +24,17 @@ add_filter('rank_math/is_post_type_accessible', function ($is_accessible, $post_
     if ($post_type === $cpt) { return true; }
     return $is_accessible;
 }, 10, 2);
+
+add_filter('rank_math/excluded_post_types', function ($post_types) {
+    if (defined('WP_DEBUG') && WP_DEBUG && current_user_can('manage_options') && is_array($post_types)) {
+        static $logged = [];
+        foreach ($post_types as $slug) {
+            if (!is_string($slug) || post_type_exists($slug)) { continue; }
+            if (isset($logged[$slug])) { continue; }
+            $logged[$slug] = true;
+            $uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
+            error_log(sprintf('[TMW-RM-AUDIT] invalid_post_type=%s source=rank_math/excluded_post_types uri=%s', $slug, $uri));
+        }
+    }
+    return $post_types;
+}, 999);
