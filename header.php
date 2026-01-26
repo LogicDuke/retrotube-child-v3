b062edb9571b3eacf9fffe2ce936bb8bb6a8188c
LogicDuke <fierensstefan08@gmail.com>
2026-01-26 17:47:09 +0100

    [TMW-A11Y] Allow zoom by fixing viewport meta (remove user-scalable/max-scale)

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+<meta content='width=device-width, initial-scale=1.0' name='viewport' />
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <?php
 	$favicon       = xbox_get_field_value( 'wpst-options', 'favicon' );
