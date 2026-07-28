27d739a224a0dae5a733f93cbbc5ffe8795ff8b4
LogicDuke <fierensstefan08@gmail.com>
2026-07-28 20:43:07 +0200

    Normalize Slot Banner enabled meta

--- inc/admin/tmw-slot-banner-metabox.php
+++ inc/admin/tmw-slot-banner-metabox.php
@@ -135,7 +135,7 @@ function tmw_render_slot_banner_metabox($post)
             var owns = Object.prototype.hasOwnProperty;
 
             if (enabled && owns.call(meta, '_tmw_slot_enabled')) {
-                enabled.checked = meta._tmw_slot_enabled === '1';
+                enabled.checked = String(meta._tmw_slot_enabled) === '1';
             }
 
             if (owns.call(meta, '_tmw_slot_mode') &&
